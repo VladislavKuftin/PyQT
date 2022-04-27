76c52db5dcd476d656fb633e74d1eec2dcc85f22
Kuftin Vladislav <boer5678@gmail.com>
2022-04-27 11:06:28 +0300

    Homework 3 done

---
+++ Lesson3/common/variables.py
@@ -0,0 +1,34 @@
+import logging
+
+
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+LOGGING_LEVEL = logging.DEBUG
+SERVER_DATABASE = 'sqlite:///server_base.db3'
+
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+SENDER = 'from'
+DESTINATION = 'to'
+
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
+EXIT = 'exit'
+
+
+RESPONSE_200 = {RESPONSE: 200}
+RESPONSE_400 = {
+            RESPONSE: 400,
+            ERROR: None
+        }
+
